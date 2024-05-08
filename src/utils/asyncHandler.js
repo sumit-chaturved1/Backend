074633cb78a59a4413f79e6e2a207a24f38a2f1f
Sumit <Chaturvedisumit338@gmail.com>
2024-05-08 21:27:11 +0530

    Add cloudinary and multer

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,6 +8,7 @@ export { asyncHandler };
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //   try {
+//     await fn(req, res, next)
 //   } catch (error) {
 //     res.status(error.code || 500).json({
 //       Success: false,
