53697ecaf030f99c26812f73b5ca0306c74d90b6
Sumit <Chaturvedisumit338@gmail.com>
2024-05-08 22:33:10 +0530

    complete setip configuration

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`⚙️ App is listning on port ${process.env.PORT || 8000}`);
+      console.log(`⚙️ App is listning on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
